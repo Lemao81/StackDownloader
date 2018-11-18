8b9e97c260f69cfa366589b7f6402e4f8c08633c
Lemao81 <jr.berlin@web.de>
2018-11-18 15:28:11 +0100

    Update dependencies + gradle version + adjustments

---
+++ buildSrc/src/main/java/Project.kt
@@ -0,0 +1,38 @@
+object App {
+    const val applicationId = "com.jueggs.stackdownloader"
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val compileSdkVersion = 28
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+}
+
+object Module {
+}
+
+object BuildType {
+    const val release = "release"
+    const val debug = "debug"
+}
+
+object Dimension {
+    const val main = "main"
+}
+
+object Flavors {
+    const val dev = "dev"
+    const val prod = "prod"
+}
+
+object Paths {
+    const val baseProject = "base-project.gradle"
+    const val baseAndroidLibrary = "../base-android-library.gradle"
+    const val baseJavaLibrary = "../base-java-library.gradle"
+    const val baseKotlinAndroidExtension = "../base-kotlin-android-extension.gradle"
+    const val baseKapt = "../base-kapt.gradle"
+    const val baseDatabinding = "../base-databinding.gradle"
+}
+
+object ProjectExtension {
+    const val android = "android"
+}
